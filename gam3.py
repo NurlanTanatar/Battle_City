9635132a42b491225a2a2eac23d4bf70d4b732f9
NurlanTanatar <nurlantanatar79@gmail.com>
2020-05-02 01:30:00 +0600

    endterm_thing

--- gam3.py
+++ gam3.py
@@ -8,6 +8,9 @@ from globalVars import screen
 pygame.init()
 bground = pygame.image.load('bground.jpg') #loading background image
 gameover = pygame.image.load('gameover.jpg') #loading imaga for gameover screen
+turn = pygame.mixer.Sound("turn.wav")
+hit = pygame.mixer.Sound("probitie.wav")
+destruction = pygame.mixer.Sound("destruction.wav")
 mainloop = True #to start loop
 tank1 = Tank('jaguar', 15,300, 300, 1, (255, 0, 0),3) #first tank (name,score's pos, tank's x, tank's y, speed, tuple for color, lives, default keys:arrows )
 tank2 = Tank('T-34', 50,100, 100, 1, (0, 0, 255),3, pygame.K_d, pygame.K_a, pygame.K_w, pygame.K_s) # second tank, but keys are wasd
@@ -15,6 +18,7 @@ bullet1 = Bullet(tank1.x + int(tank1.width / 2), tank1.y + int(tank1.width / 2),
 bullet2 = Bullet(tank2.x + int(tank2.width / 2), tank2.y + int(tank2.width / 2),(0,120,255)) # ↑
 tanks = [tank1,tank2] #adding our instances to list
 bullets = [bullet1,bullet2] # ↑
+
 
 #_____________________________________________starting main loop of the game____________________________
 while mainloop:
@@ -34,12 +38,15 @@ while mainloop:
                     bullet2.shoot (tank2)  
             for tank in tanks: # tracking whenever is button of direction is clicked by tank
                 if event.key in tank.KEY.keys():
+                    turn.play()
                     tank.change_direction(tank.KEY[event.key])
     if bullet1.colission(tank2) == True: #if bullet of tank1 hitted tank2
+        hit.play()
         tank2.lives -= 1 #decrementing 1 life
         bullet1.drop = False # "disappearing" bullet
         bullet1.x, bullet1.y = tank1.x + int(tank1.width / 2), tank1.y + int(tank1.width / 2) #"giving back" bullet to the center of tank1
     if bullet2.colission(tank1) == True: #same thing from prev but vica versa
+        hit.play()
         tank1.lives -= 1
         bullet2.drop = False
         bullet2.x, bullet2.y = tank2.x + int(tank2.width / 2), tank2.y + int(tank2.width / 2)
@@ -54,6 +61,7 @@ while mainloop:
         tank.move() #move it in some direction
         tank.chances() #blitting lives' info which dependa on tank's left lives
         if tank.lives == 0: #if tank got zero lives
+            destruction.play()
             bground=gameover #blitting background image by gameover image
             tank.dead() # just blitting over background image info which tank is destructed
             tank1.speed, tank2.speed = 0, 0 #stopping tanks
